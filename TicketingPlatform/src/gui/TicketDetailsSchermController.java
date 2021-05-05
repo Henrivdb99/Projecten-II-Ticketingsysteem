eb7971fa34ae9273a28088bd6bd31b4d33191320
Korneel <korneel.meyers@gmail.com>
2021-05-05 16:29:12 +0200

    Added TicketDetailsScherm

---
+++ TicketingPlatform/src/gui/TicketDetailsSchermController.java
@@ -0,0 +1,41 @@
+package gui;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Label;
+
+public class TicketDetailsSchermController {
+	@FXML
+	private Label lblDatumAangemaakt;
+	@FXML
+	private Label lblTitel;
+	@FXML
+	private Label lblType;
+	@FXML
+	private Label lblAdres;
+	@FXML
+	private Label lblVasteLijnWerk;
+	@FXML
+	private Label lblGsmNummer;
+	@FXML
+	private Label lblTijdInDienst;
+	@FXML
+	private Label lblKlantnummer;
+	@FXML
+	private Label lblNaamContactpersoon;
+	@FXML
+	private Label lblOmschrijving;
+	@FXML
+	private Label lblOpmerkingen;
+	@FXML
+	private Label lblToegewezenTechnieker;
+	@FXML
+	private Label lblVoornaamContactpersoon;
+	@FXML
+	private Label lblEmailAdres;
+	@FXML
+	private Label lblGsmnummer;
+	@FXML
+	private Label lblVastelijn;
+
+}
