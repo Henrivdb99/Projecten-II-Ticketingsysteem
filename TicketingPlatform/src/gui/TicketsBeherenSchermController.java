0b1ac7e7050606ee6be99ede04204663285c5690
Korneel <korneel.meyers@gmail.com>
2021-05-06 19:02:21 +0200

    Fix cboStatus null

--- TicketingPlatform/src/gui/TicketsBeherenSchermController.java
+++ TicketingPlatform/src/gui/TicketsBeherenSchermController.java
@@ -21,7 +21,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.TableColumn;
 
-public class TicketsBeherenSchermController extends BorderPane implements Initializable{
+public class TicketsBeherenSchermController extends BorderPane implements Initializable {
 	@FXML
 	private Button btnTicketToevoegen;
 	@FXML
@@ -42,12 +42,13 @@ public class TicketsBeherenSchermController extends BorderPane implements Initia
 	private TableColumn colKlant;
 	@FXML
 	private TableColumn colStatus;
-	
+
 	private DashboardSchermController parent;
 	private AangemeldeGebruikerController ac;
-	private Ticket selectedTicket; 
+	private Ticket selectedTicket;
 
-	public TicketsBeherenSchermController(DashboardSchermController dashboardSchermController, AangemeldeGebruikerController ac) {
+	public TicketsBeherenSchermController(DashboardSchermController dashboardSchermController,
+			AangemeldeGebruikerController ac) {
 		this.parent = dashboardSchermController;
 		this.ac = ac;
 		try {
@@ -61,39 +62,47 @@ public class TicketsBeherenSchermController extends BorderPane implements Initia
 
 		} catch (IOException ex) {
 			throw new RuntimeException(ex);
-		}	}
+		}
+	}
+
 	// Event Listener on Button[#btnTicketToevoegen].onAction
 	@FXML
 	public void btnTicketToevoegenOnAction(ActionEvent event) {
 		TicketToevoegenSchermController ttsc = new TicketToevoegenSchermController(this, ac);
-		this.setRight(ttsc);	}
+		this.setRight(ttsc);
+	}
+
 	// Event Listener on Button[#btnTicketWijzigen].onAction
 	@FXML
 	public void btnTicketWijzigenOnAction(ActionEvent event) {
 		this.selectedTicket = tblView.getSelectionModel().getSelectedItem();
 		TicketWijzigenSchermController twsc = new TicketWijzigenSchermController(this, selectedTicket, ac);
-		this.setRight(twsc);	
-		}
+		this.setRight(twsc);
+	}
+
 	// Event Listener on Button[#btnTerug].onAction
 	@FXML
 	public void btnTerugOnAction(ActionEvent event) {
 		Stage stage = (Stage) (getScene().getWindow());
 		stage.setScene(this.parent.getScene());
-		}
+	}
+
 	// Event Listener on Button[#btnTicketDetails].onAction
 	@FXML
 	public void btnTicketDetailsOnAction(ActionEvent event) {
 		this.selectedTicket = tblView.getSelectionModel().getSelectedItem();
-		TicketDetailsSchermController tdsc = new TicketDetailsSchermController(this,
-				this.selectedTicket);
+		TicketDetailsSchermController tdsc = new TicketDetailsSchermController(this, this.selectedTicket);
 
-		this.setRight(tdsc);	}
+		this.setRight(tdsc);
+	}
+
 	// Event Listener on TableView[#tblView].onMouseClicked
 	@FXML
 	public void userClickedOnTable(MouseEvent event) {
 		btnTicketDetails.setDisable(false);
 		btnTicketWijzigen.setDisable(false);
-		}
+	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		colTicketId.setCellValueFactory(new PropertyValueFactory<Ticket, Integer>("ticketId"));
@@ -101,6 +110,6 @@ public class TicketsBeherenSchermController extends BorderPane implements Initia
 		colTicketTitel.setCellValueFactory(new PropertyValueFactory<Ticket, String>("titel"));
 		colKlant.setCellValueFactory(new PropertyValueFactory<Klant, String>("naamVoornaam"));
 		colStatus.setCellValueFactory(new PropertyValueFactory<Ticket, String>("status"));
-		tblView.setItems(ac.geefWerknemers());		
+		tblView.setItems(ac.geefWerknemers());
 	}
 }
