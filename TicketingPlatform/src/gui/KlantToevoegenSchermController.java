7e7b6962c10d8f16c6b3bc9c8054390ea55fa8d8
LeonPaelinck <60927864+LeonPaelinck@users.noreply.github.com>
2021-05-12 14:36:53 +0200

    klanten GUI gefixt

--- TicketingPlatform/src/gui/KlantToevoegenSchermController.java
+++ TicketingPlatform/src/gui/KlantToevoegenSchermController.java
@@ -1,26 +1,19 @@
 package gui;
 
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 
 import domein.controllers.AangemeldeGebruikerController;
-import domein.controllers.AdministratorController;
-import domein.models.TypeGebruiker;
-import javafx.collections.FXCollections;
+import domein.models.GebruikerStatus;
 import javafx.event.ActionEvent;
-
-import javafx.scene.control.Label;
-
-import javafx.scene.control.PasswordField;
-
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class KlantToevoegenSchermController extends GridPane {
 	@FXML
@@ -29,6 +22,8 @@ public class KlantToevoegenSchermController extends GridPane {
 	private Button btnKlantAanmaken;
 	@FXML
 	private Label lblFout;
+	@FXML
+	private Label lblStatus;
 	@FXML
 	private TextField txfNaam;
 	@FXML
@@ -53,6 +48,8 @@ public class KlantToevoegenSchermController extends GridPane {
 	private PasswordField txfWachtwoord;
 	@FXML
 	private PasswordField txfWachtwoordBevestigen;
+	@FXML
+	private  ChoiceBox<GebruikerStatus>  cboStatus;
 
 	private KlantenBeherenSchermController parent;
 	private AangemeldeGebruikerController ac;
@@ -67,6 +64,10 @@ public class KlantToevoegenSchermController extends GridPane {
 			loader.setRoot(this);
 			loader.setController(this);
 			loader.load();
+			
+			//status wordt niet ingevuld door gebruiker bij aanmaken
+			cboStatus.setVisible(false); 
+			lblStatus.setVisible(false);
 
 		} catch (IOException ex) {
 			throw new RuntimeException(ex);
@@ -84,16 +85,20 @@ public class KlantToevoegenSchermController extends GridPane {
 	@FXML
 	public void btnKlantAanmakenOnAction(ActionEvent event) {
 
-		String samengesteldAdres = txfStraat.getText() + " " + txfHuisnummer.getText() + ", " + txfPostcode.getText()
-				+ " " + txfStad.getText();
+		//String samengesteldAdres = txfStraat.getText() + " " + txfHuisnummer.getText() + ", " + txfPostcode.getText()
+		//		+ " " + txfStad.getText();
 
 		try {
 
 			if (txfWachtwoord.getText().equals(txfWachtwoordBevestigen.getText())) {
 				if (!txfStraat.getText().isBlank() || !txfHuisnummer.getText().isBlank()
 						|| !txfPostcode.getText().isBlank() || !txfStad.getText().isBlank()) {
+					
 					ac.voegKlantToe(txfNaam.getText(), txfVoornaam.getText(), txfEmail.getText(), new String[] {txfGsmNummer.getText(), txfVasteLijn.getText()}, txfWachtwoord.getText(),
 							new String[] {txfStraat.getText(), txfHuisnummer.getText(), txfPostcode.getText(), txfStad.getText()}, txfBedrijfsnaam.getText());
+					
+					resetVelden();
+					lblFout.setText("Aanmaken klant gelukt!");
 				} else {
 					lblFout.setText("Gelieve alle adresgegevens correct in te vullen.");
 				}
@@ -105,5 +110,23 @@ public class KlantToevoegenSchermController extends GridPane {
 			lblFout.setText(e.getMessage());
 		}
 
+	}
+	
+	private void resetVelden() {		
+
+		txfNaam.clear();
+		txfVoornaam.clear();
+		txfEmail.clear();
+		txfGsmNummer.clear();
+		txfVasteLijn.clear();
+		
+		txfWachtwoord.clear();
+		txfWachtwoordBevestigen.clear();
+		
+		txfBedrijfsnaam.clear();
+		txfStraat.clear();
+		txfHuisnummer.clear();
+		txfPostcode.clear();
+		txfStad.clear();
 	}
 }
